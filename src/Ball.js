017b33d8518ab2854fc6bd0dfd389d0b6ce71678
MPI <mpierobon@sequel.com>
2020-05-05 22:27:50 +0100

    Allow speed up to 200% and make sure at 0% the balls do not bulge

--- src/Ball.js
+++ src/Ball.js
@@ -82,7 +82,8 @@ export class Ball {
       const dx = otherBallX - this.x
       const dy = otherBallY - this.y
 
-      if (checkCollision({ dx, dy, diameter: BALL_RADIUS * 2 })) {
+      if (this.isAtLeastOneOfTheTwoBallsMoving(otherBall) &&
+        (checkCollision({ dx, dy, diameter: BALL_RADIUS * 2 }))) {
         const { ax, ay } = calculateChangeDirection({ dx, dy })
 
         // apply the movement just to balls that can move
@@ -124,6 +125,10 @@ export class Ball {
         }
       }
     }
+  }
+
+  isAtLeastOneOfTheTwoBallsMoving(otherBall) {
+    return (otherBall.vx !== 0 || otherBall.vy !== 0 || this.vx !== 0 || this.vy !== 0)
   }
 
   isAwareToBeInfected (otherBall) {
